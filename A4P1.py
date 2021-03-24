e5579c3b7605e8b1d2eca7e03540b581170b1dd9
Ahmad <aamin1@ualberta.ca>
2021-03-24 17:40:55 -0600

    a4p1 working

--- A4P1.py
+++ A4P1.py
@@ -2,13 +2,30 @@
 Winter 2021 Cmput 291
 Assignment 4, Part 1
 Ahmad, Chris
-
-Query 1: ____________________________________________________________
-Query 2: ____________________________________________________________
 '''
-from averager import avgEachDb
-    
+from averager import *
+
+def makeIndex(aBool):
+    dbList = ["A4v100.db", "A4v1k.db","A4v10k.db","A4v100k.db","A4v1M.db"]
+    for db in dbList:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        if aBool:
+            c.execute("CREATE INDEX idxNeedsPart ON Parts (needsPart)")
+            print(f"{db}: Index has been created.")
+        else:
+            c.execute("DROP INDEX IF EXISTS idxNeedsPart")
+            print(f"{db}: Index has been dropped, if it existed.")
+        conn.close()
+
+Q1 = ["SELECT partPrice FROM Parts WHERE partNumber = :code;", "partNumber", 1]
+Q2 = ["SELECT partPrice FROM Parts WHERE needsPart = :code;", "needsPart", 2]
+
 if __name__ == "__main__":
-    #example
-    Query1 = "select COUNT(*) from Parts"
-    print(avgEachDb(Query1, 100,100,100,100,100))
+    makeIndex(False)
+    avgEachDb(Q1[0],100,100,100,100,100,Q1[1], Q1[2])
+    avgEachDb(Q2[0],100,100,100,100,100,Q2[1], Q2[2])
+    makeIndex(True)
+    avgEachDb(Q1[0],100,100,100,100,100,Q1[1], Q1[2])
+    avgEachDb(Q2[0],100,100,100,100,100,Q2[1], Q2[2])
+
--- averager.py
+++ averager.py
@@ -6,6 +6,14 @@ Ahmad, Chris
 import sqlite3
 import sys
 import time
+import random
+
+def getChoiceList(column, c):
+    '''
+    gives a random element of column from database.
+    '''
+    c.execute(f"Select {column} from Parts")
+    return c.fetchall()
 
 def check(n, c):
     '''
@@ -14,67 +22,40 @@ def check(n, c):
     c.execute("select COUNT(*) from Parts")
     if c.fetchone()[0] != n:
         sys.exit("DATABASE SIZE INCORRECT! You are either using the wrong database or have corrupted the correct database.")
-
-def avgEachDb(strng, numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTimesDB5):
+    
+def avgEachDb(strng, numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTimesDB5, column, n):
     '''
     inputs:
-        strng: an sql query.
-        numTimesDBX: the number of trials over which to avg running time of strng against X Database.
+        (string) strng: sql query
+        (integer) numTimesDBX: the number of trials over which to avg running time of strng against X Database.
+        (string) column: the column we randomly select from to get data to put into strng.
 
     returns:
         tuple where index i gives the average time for running strng against the i+1th database over numTimesDB(i+1) trials.
     '''
 
-    ans = [0, 0, 0, 0, 0]
-
-    def func(c):
+    def timeNeeded(c, choice):
+        '''
+        returns time to run query "strng" against database c
+        '''
         startTime = time.time()
-        c.execute(strng)
+        c.execute(strng,{'code': choice}) 
         endTime = time.time()
         return(endTime - startTime)
 
-    conn = sqlite3.connect('A4v100.db')
-    c = conn.cursor()
-    check(10**2, c)
-    for _ in range(numTimesDB1):
-        ans[0] += func(c)
-    conn.close()
+    dbList = ["A4v100.db", "A4v1k.db","A4v10k.db","A4v100k.db","A4v1M.db"]
+    dbNumTimesList = [numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTimesDB5]
+    ans = [0, 0, 0, 0, 0]
 
-    conn = sqlite3.connect('A4v1k.db')
-    c = conn.cursor()
-    check(10**3, c)
-    for _ in range(numTimesDB2):
-        ans[1] += func(c)
-    conn.close()
-
-    conn = sqlite3.connect('A4v10k.db')
-    c = conn.cursor()
-    check(10**4, c)
-    for _ in range(numTimesDB3):
-        ans[2] += func(c)
-    conn.close()
-
-    conn = sqlite3.connect('A4v100k.db')
-    c = conn.cursor()
-    check(10**5, c)
-    for _ in range(numTimesDB4):
-        ans[3] += func(c)
-    conn.close()
-
-    conn = sqlite3.connect('A4v1M.db')
-    c = conn.cursor()
-    check(10**6, c)
-    for _ in range(numTimesDB5):
-        ans[4] += func(c)
-    conn.close()
-
-    ans[0] /= numTimesDB1
-    ans[1] /= numTimesDB2
-    ans[2] /= numTimesDB3
-    ans[3] /= numTimesDB4
-    ans[4] /= numTimesDB5
-
-    return tuple(ans)
-
-if __name__ == "__main__":
-    pass
+    for i in range(len(dbList)):
+        conn = sqlite3.connect(dbList[i])
+        c = conn.cursor()
+        dbSize = 10**(i+2)
+        check(dbSize, c)
+        optionList = getChoiceList(column, c)
+        for _ in range(dbNumTimesList[i]):
+            choice = random.choice(optionList)[0]
+            ans[i] += timeNeeded(c, choice)
+        conn.close()
+        ans[i] /= dbNumTimesList[i]
+        print(f"{dbList[i]}: Ran Query {n} {dbNumTimesList[i]} times and got {ans[i]*1000} ms on average")
