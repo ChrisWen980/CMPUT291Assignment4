22ace8058f132e40a1e8b45d9e70d882b7343f98
Ahmad <aamin1@ualberta.ca>
2021-03-24 18:13:17 -0600

    a4p2 ready

--- A4P2.py
+++ A4P2.py
@@ -3,9 +3,26 @@ Winter 2021 Cmput 291
 Assignment 4, Part 2
 Ahmad, Chris
 '''
-from averager import avgEachDb
-    
+from averager import *
+
+def makeIndex(aBool):
+    dbList = ["A4v100.db", "A4v1k.db","A4v10k.db","A4v100k.db","A4v1M.db"]
+    for db in dbList:
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        if aBool:
+            c.execute("CREATE INDEX idxMadeIn ON Parts (madeIn)")
+            print(f"{db}: Index has been created.")
+        else:
+            c.execute("DROP INDEX IF EXISTS idxMadeIn")
+            print(f"{db}: Index has been dropped, if it existed.")
+        conn.close()
+
+Q3 = ["SELECT AVG(partPrice) FROM Parts GROUP BY madeIn;", 3]
+
 if __name__ == "__main__":
-    #example
-    Query1 = "select COUNT(*) from Parts"
-    print(avgEachDb(Query1, 100,100,100,100,100))
+    makeIndex(False)
+    avgEachDbSimple(Q3[0],100,100,100,100,100,Q3[1])
+    makeIndex(True)
+    avgEachDbSimple(Q3[0],100,100,100,100,100,Q3[1])
+    makeIndex(False)
--- averager.py
+++ averager.py
@@ -15,14 +15,56 @@ def getChoiceList(column, c):
     c.execute(f"Select {column} from Parts")
     return c.fetchall()
 
-def check(n, c):
+def checkDBSize(n, c):
     '''
     enforces connected database has cardinality n.
     '''
     c.execute("select COUNT(*) from Parts")
     if c.fetchone()[0] != n:
         sys.exit("DATABASE SIZE INCORRECT! You are either using the wrong database or have corrupted the correct database.")
-    
+
+def timeNeeded(strng, c, choice):
+    '''
+    returns time to run query "strng" against database c
+    '''
+    startTime = time.time()
+    c.execute(strng,{'code': choice}) 
+    endTime = time.time()
+    return(endTime - startTime)
+
+def timeNeededSimple(strng, c):
+    startTime = time.time()
+    c.execute(strng)
+    endTime = time.time()
+    return(endTime - startTime)
+
+
+def avgEachDbSimple(strng, numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTimesDB5, n):
+    '''
+    inputs:
+        (string) strng: sql query
+        (integer) numTimesDBX: the number of trials over which to avg running time of strng against X Database.
+        (string) column: the column we randomly select from to get data to put into strng.
+
+    returns:
+        tuple where index i gives the average time for running strng against the i+1th database over numTimesDB(i+1) trials.
+    '''
+
+    dbList = ["A4v100.db", "A4v1k.db","A4v10k.db","A4v100k.db","A4v1M.db"]
+    dbNumTimesList = [numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTimesDB5]
+    ans = [0, 0, 0, 0, 0]
+
+    for i in range(len(dbList)):
+        conn = sqlite3.connect(dbList[i])
+        c = conn.cursor()
+        dbSize = 10**(i+2)
+        checkDBSize(dbSize, c)
+        for _ in range(dbNumTimesList[i]):
+            ans[i] += timeNeededSimple(strng, c)
+        conn.close()
+        ans[i] /= dbNumTimesList[i]
+        print(f"{dbList[i]}: Ran Query {n} {dbNumTimesList[i]} times and got {ans[i]*1000} ms on average")
+
 def avgEachDb(strng, numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTimesDB5, column, n):
     '''
     inputs:
@@ -34,15 +76,6 @@ def avgEachDb(strng, numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTime
         tuple where index i gives the average time for running strng against the i+1th database over numTimesDB(i+1) trials.
     '''
 
-    def timeNeeded(c, choice):
-        '''
-        returns time to run query "strng" against database c
-        '''
-        startTime = time.time()
-        c.execute(strng,{'code': choice}) 
-        endTime = time.time()
-        return(endTime - startTime)
-
     dbList = ["A4v100.db", "A4v1k.db","A4v10k.db","A4v100k.db","A4v1M.db"]
     dbNumTimesList = [numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTimesDB5]
     ans = [0, 0, 0, 0, 0]
@@ -51,11 +84,11 @@ def avgEachDb(strng, numTimesDB1, numTimesDB2, numTimesDB3, numTimesDB4, numTime
         conn = sqlite3.connect(dbList[i])
         c = conn.cursor()
         dbSize = 10**(i+2)
-        check(dbSize, c)
+        checkDBSize(dbSize, c)
         optionList = getChoiceList(column, c)
         for _ in range(dbNumTimesList[i]):
             choice = random.choice(optionList)[0]
-            ans[i] += timeNeeded(c, choice)
+            ans[i] += timeNeeded(strng, c, choice)
         conn.close()
         ans[i] /= dbNumTimesList[i]
         print(f"{dbList[i]}: Ran Query {n} {dbNumTimesList[i]} times and got {ans[i]*1000} ms on average")
